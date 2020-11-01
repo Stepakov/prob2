a8dc5ecb3031b7342de7b726d45d8dd4cf3349f5
sasha stepakov <sanechkastepakov@gmail.com>
2020-11-01 13:31:35 +0200

    add version

--- first.py
+++ first.py
@@ -1,1 +1,18 @@
-print( 'hell' )
+import sys
+import platform
+
+info = "OS info is \n{}\nPython version is {} {}".format(
+    platform.uname(), sys.version, platform.architecture()
+)
+
+info2 = "Windows: {}\nArchitecture: {}\nSystem: {}\nPython version: {}".format(
+    platform.uname(), platform.architecture(), platform.version(),
+    sys.version
+)
+
+print( info )
+print( '\n\n' )
+print( info2 )
+
+with open( 'os_info.txt', 'w' ) as ff:
+    ff.write( info2 )
